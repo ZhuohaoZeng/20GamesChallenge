55a88e95ee8e03359c3b07d768bdde55d9244bce
ZhuohaoZeng <Zhuohao_Zeng@unc.edu>
2025-09-20 00:32:43 +0100

    Refactor using stragy method and items from effective c++

--- pong/main.cpp
+++ pong/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "include/raylib.h"
 #include <string>
+#include <memory>
 
 struct Bounds
 {
@@ -12,11 +13,12 @@ struct Input
     bool up{};
     bool down{};
 };
+class Controller {
+public:
+    virtual Vector2 Move() = 0; 
+    virtual ~Controller() = default;
+};
 
-//----Below should be in the header files, but I didn't because I am lazy --------
-class Ball;
-class paddle; 
-//----End of the header --------
 class Ball{
 public:
     Ball(float x, float y, float r, Color c, Vector2 bs): 
@@ -47,18 +49,25 @@ private:
 
 class paddle{
 public:
-    paddle(int posX, int poxY, int width, int height, Color color, Vector2 ms): 
-          posX(posX), posY(poxY), width(width), height(height), color(color), ms(ms){}
+    paddle(int posX, int poxY, int width, int height, Color color, Vector2 m, std::unique_ptr<Controller> ctrl = nullptr): 
+          posX(posX), posY(poxY), width(width), height(height), color(color), ms(m), controller(std::move(ctrl)){}
     Color getColor() const {return color;}
     Vector2 getMovSpe() const{return ms;}
     Vector2 getPosition() const{return {(float) posX,(float) posY};}
     void setY(float posY){this->posY = posY;}
+    void setController(std::unique_ptr<Controller> ctrl) {controller = std::move(ctrl);}
     void setPosition(Vector2 position)
     {
         posX = position.x;
         posY = position.y;
     }
-    void moveBy(float ms, float my) {posX += ms; posY+= my;}
+    //Get Move direction from the inputted Controller comonent
+    void move() {
+        Vector2 m{controller->Move()};
+        posX += m.x; posY+= m.y;
+        clampY(0, 620);
+    }
+
     void clampY(int minY, int maxY)
     {
         if (posY < minY) posY = minY;
@@ -74,29 +83,44 @@ private:
     int height;
     Color color;
     Vector2 ms;
+    std::unique_ptr<Controller> controller;
 };
 
-class enemy : public paddle{
+class PlayerController: public Controller {
 public:
-    enemy(int posX, int poxY, int width, int height, Color color, Vector2 ms):
-    paddle(posX, poxY, width, height, color, ms){}
-    void Move(Ball& ball){
-        if (ball.getPosition().y > getPosition().y)
-        {
-            setY(getPosition().y + getMovSpe().y);
-        }
-        else if (ball.getPosition().y < getPosition().y) 
-        {
-            setY(getPosition().y - getMovSpe().y);
-        }
+    PlayerController(paddle& p): pd(p){}
+    Vector2 Move() override
+    {
+        Input in{IsKeyDown(KEY_UP), IsKeyDown(KEY_DOWN)};
+        if (in.up) return Vector2{0, -pd.getMovSpe().y};
+        else if (in.down) return Vector2{0, pd.getMovSpe().y};
+        else return Vector2{0, 0};
     }
 private:
-    
-    
+    paddle& pd;
 };
 
+class EnemyController: public Controller {
+public:
+    EnemyController(paddle& p, Ball& b): pd(p), ball(b){}
+    Vector2 Move() override
+    {
+        if (ball.getPosition().y > pd.getPosition().y)
+        {
+            return Vector2{0, pd.getMovSpe().y};
+        }
+        else if (ball.getPosition().y < pd.getPosition().y) 
+        {
+            return Vector2{0, -pd.getMovSpe().y};
+        }
+        else return Vector2{0, 0};
+    }
+private:
+    paddle& pd; 
+    Ball& ball; 
+};
 
-void update(Ball& ball, const paddle& p, const Bounds& bounds)
+void checkCollision(Ball& ball, const paddle& p, const Bounds& bounds)
 {
     Vector2 ballP = ball.getPosition();
     
@@ -112,7 +136,6 @@ void update(Ball& ball, const paddle& p, const Bounds& bounds)
         ball.respond();
     }
     ball.moveBy(ball.getSpeed().x, ball.getSpeed().y);
-    
 }
 // balls non-member method
 void draw(const Ball& b)
@@ -121,14 +144,7 @@ void draw(const Ball& b)
     DrawCircle(centerX, centerY, b.getRadius(), b.getColor());
 }
 
-//non-member nonfriend paddle function
-void update(paddle& p, const Input& i, const Bounds& b)
-{
-    if (i.up) p.moveBy(0, -p.getMovSpe().y);
-    if (i.down) p.moveBy(0, p.getMovSpe().y);
-    p.clampY(0, 620);
-    return;
-};
+
 // paddles non-member method
 void draw(const paddle& p)
 {
@@ -144,21 +160,22 @@ int main(void)
     Vector2 ballSpeed{ 3.0f, 2.0f };
     Ball ball{(float) bounds.w / 2, (float) bounds.h / 2, 30, WHITE, ballSpeed};
     paddle player{70, bounds.h/2, 30, 100, WHITE, {0, 4.0f}};
-    enemy enemy{1200, bounds.h/2, 30, 100, WHITE, {0, 4.0f}};
+    player.setController(std::make_unique<PlayerController>(player));
+    paddle enemy{1200, bounds.h/2, 30, 100, WHITE, {0, 4.0f}};
+    enemy.setController(std::make_unique<EnemyController>(enemy, ball));
     InitWindow(bounds.w, bounds.h, "A new Winodw");
     SetTargetFPS(120); 
     //Main Game Loop
     while(!WindowShouldClose())
     {
-        Input in{IsKeyDown(KEY_UP), IsKeyDown(KEY_DOWN)};
         BeginDrawing();
         ClearBackground(BLACK);
-        update(player, in, bounds);
+        player.move();
         draw(player);
-        update(ball, player, bounds);
-        update(ball, enemy, bounds);
+        checkCollision(ball, player, bounds);
+        checkCollision(ball, enemy, bounds);
         draw(ball);
-        enemy.Move(ball);
+        enemy.move();
         draw(enemy);
         // if (ball.scoreCheck(player)) ++player_score;
         // if (ball.scoreCheck(enemy)) ++enemy_score;
